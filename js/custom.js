da72b947ef8d409fae84eb32c9c841f8ac79f7bb
tariqsulley <32623579+tariqsulley@users.noreply.github.com>
2020-07-20 12:57:45 +0000

    Add files via upload

---
+++ js/custom.js
@@ -0,0 +1,38 @@
+(function ($) {
+
+  "use strict";
+
+    $(window).load(function(){
+      $('.preloader').fadeOut(1000);   
+    });
+
+
+    $('.navbar-collapse a').on('click',function(){
+      $(".navbar-collapse").collapse('hide');
+    });
+
+    $(window).scroll(function() {
+      if ($(".navbar").offset().top > 50) {
+        $(".navbar-fixed-top").addClass("top-nav-collapse");
+          } else {
+            $(".navbar-fixed-top").removeClass("top-nav-collapse");
+          }
+    });
+
+
+    $.stellar({
+      horizontalScrolling: false,
+    }); 
+
+
+    $(function() {
+      $('.custom-navbar a').on('click', function(event) {
+        var $anchor = $(this);
+          $('html, body').stop().animate({
+            scrollTop: $($anchor.attr('href')).offset().top - 49
+          }, 1000);
+            event.preventDefault();
+      });
+    });  
+
+})(jQuery);
